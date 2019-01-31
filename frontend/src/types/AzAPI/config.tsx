86c87c59b55dec8d1ee8d6e4afb70a2410e93421
Caleb Reiter <caleb@reiters.org>
2019-01-31 09:47:05 -0600

    added azure api config

---
+++ frontend/src/types/AzAPI/config.tsx
@@ -0,0 +1,27 @@
+export interface IAzConfig {
+    apiKey: string,
+    apiVer: string,
+    method: 'GET' | 'POST',
+    protocol: string,
+    serviceDomain: string,
+    serviceName: string,
+    servicePath: string,
+}
+
+export const defaultAzConfig: IAzConfig  = {
+    apiKey: process.env.API_KEY || process.env.REACT_APP_API_KEY || '',
+    apiVer: process.env.API_VERSION || process.env.REACT_APP_API_VERSION || '',
+    method: 'GET',
+    protocol: 'https',
+    serviceDomain: process.env.SERVICE_DOMAIN || process.env.REACT_APP_SERVICE_DOMAIN || '',
+    serviceName: process.env.SERVICE_NAME || process.env.REACT_APP_SERVICE_NAME || '',
+    servicePath: process.env.SERVICE_PATH || process.env.REACT_APP_SERVICE_PATH || '',
+}
+
+export const parseConfig = (config: IAzConfig) => {
+    const root = `${config.protocol}://${config.serviceName}.${config.serviceDomain}/`;
+    const path = `${config.servicePath}?api-version=${config.apiVer}`;
+    return root + path;
+}
+
+export const defaultParsedConfig: string = parseConfig(defaultAzConfig);
